3466203dc88526f2f33f422f405a43f4101abe4d
Sanjay <sanjay.tk@hotmail.com>
2025-11-08 19:09:34 +0530

    Added headless option

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -2,6 +2,7 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 import utils.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,7 +14,22 @@ public class BaseTest {
     public void setUp() {
         ConfigReader.loadProperties();
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        // Chrome configuration
+        ChromeOptions options = new ChromeOptions();
+//        options.addArguments("--headless=new");        // ✅ stable headless mode for Chrome 109+
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-infobars");
+
+        // Optional: detect CI environment dynamically
+        if (System.getenv("CI") != null) {
+            System.out.println("Running on CI — enabling headless mode");
+            options.addArguments("--headless=new");
+        }
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.get(ConfigReader.get("baseURL"));
     }
@@ -21,7 +37,7 @@ public class BaseTest {
     @AfterMethod
     public void tearDown() {
         if (driver != null) {
-            //driver.quit();
+            driver.quit();
         }
     }
 }
