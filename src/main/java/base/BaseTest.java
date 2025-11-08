226ba9e1e52bd4bd067e9bc0f13233ed0143e7b3
Sanjay <sanjay.tk@hotmail.com>
2025-11-08 19:13:43 +0530

    Added headless option

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -3,7 +3,8 @@ package base;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import utils.ConfigReader;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
